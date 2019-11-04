97029d4197076f9a0323adbbd37885fd3091546d
Matan Kushner <hello@matchai.me>
2019-11-04 21:34:25 +0900

    Remove unused variable in hostname

--- src/modules/hostname.rs
+++ src/modules/hostname.rs
@@ -30,8 +30,6 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         }
     };
 
-    let trim_at = module.config_value_str("trim_at").unwrap_or(".");
-
     //rustc doesn't let you do an "if" and an "if let" in the same if statement
     // if this changes in the future this can become a lot cleaner
     let host = if config.trim_at != "" {
